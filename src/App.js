bbfc4e8cb5af54be237edebfc8e6eccb69976889
himanshi4902 <sharmahimanshi0094@gmail.com>
2024-01-06 02:39:28 +0530

    styling also done

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ class App extends React.Component {
         <div className="app">
             <div className="card">
                 <h1 className="heading">{advice}</h1>
+                <button className="button" onClick={this.fetchAdvisor}>
+                    <span>GENERATE ANOTHER!!</span>
+                </button>
             </div>
         </div>
         );
